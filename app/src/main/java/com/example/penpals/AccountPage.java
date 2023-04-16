9f0c133f9c3fdf5527365852a094c84fe8976047
Shreya-Jathar <shreyajathar808@gmail.com>
2023-04-17 03:33:44 +0530

    Added data collection and display functionalities

--- app/src/main/java/com/example/penpals/AccountPage.java
+++ app/src/main/java/com/example/penpals/AccountPage.java
@@ -1,17 +1,41 @@
 package com.example.penpals;
 
+import android.app.usage.UsageEvents;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class AccountPage extends AppCompatActivity {
 
     Button logOut;
     FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    TextView name, age, country, nativeLang;
+    String UID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +44,39 @@ public class AccountPage extends AppCompatActivity {
         logOut = findViewById(R.id.logOut);
         mAuth = FirebaseAuth.getInstance();
 
+        name = findViewById(R.id.nameVal);
+        age = findViewById(R.id.ageVal);
+        country = findViewById(R.id.countryVal);
+        nativeLang = findViewById(R.id.nativeLangVal);
         logOut.setOnClickListener(view -> {
             mAuth.signOut();
             Intent intent = new Intent(AccountPage.this, MainActivity.class);
             startActivity(intent);
         });
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        mUser = mAuth.getCurrentUser();
+        UID = mUser.getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference ref = db.collection("Users").document(UID);
+
+        ref.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.getResult().exists()) {
+                            name.setText(task.getResult().getString("Name"));
+                            age.setText(task.getResult().getString("Age"));
+                            country.setText(task.getResult().getString("Country"));
+                            nativeLang.setText(task.getResult().getString("Native Language"));
+                        }
+                        else {
+                            return;
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/penpals/RegisterPage.java
+++ app/src/main/java/com/example/penpals/RegisterPage.java
@@ -2,14 +2,27 @@ package com.example.penpals;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.Attributes;
 
 public class RegisterPage extends AppCompatActivity {
 
@@ -41,6 +54,12 @@ public class RegisterPage extends AppCompatActivity {
     private void createAccount() {
         String USN = email.getText().toString();
         String PWD = password.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> newUser = new HashMap<>();
+        newUser.put("Name", name.getText().toString());
+        newUser.put("Age", age.getText().toString());
+        newUser.put("Country", country.getText().toString());
+        newUser.put("Native Language", languages.getText().toString());
 
         if(PWD.isEmpty() || PWD.length() < 8) {
             password.setError("Password length must be at least 8 characters");
@@ -49,8 +68,19 @@ public class RegisterPage extends AppCompatActivity {
             mAuth.createUserWithEmailAndPassword(USN, PWD).addOnCompleteListener(task -> {
                 if(task.isSuccessful()) {
                     mUser = mAuth.getCurrentUser();
+                    db.collection("Users").document(mUser.getUid()).set(newUser).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(RegisterPage.this, "Data Saved Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(RegisterPage.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                            Log.w("Error", e);
+                        }
+                    });
                     changeActivity();
-                    Toast.makeText(RegisterPage.this, "Registration Successful", Toast.LENGTH_SHORT).show();
                 }
                 else
                     Toast.makeText(RegisterPage.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
